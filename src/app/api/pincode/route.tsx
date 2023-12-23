3bc3a1dc8e1a7e0a8197c69f81873c9eb4081dd1
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-12-24 01:57:38 +0530

    Feature : Add pincode validation and delivery check functionality.

---
+++ src/app/api/pincode/route.tsx
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const serviceablePincodes = ["431002", "431003", "431004", "431005"]; // Example list of serviceable pincodes
+
+export function GET(req: NextRequest, res: NextResponse) {
+  const p = req.nextUrl.searchParams.get("p") || "";
+
+  return NextResponse.json({ userPincode: p, serviceablePincodes });
+}
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 import { Button } from "@/shadcn/components/ui/button";
+import { Input } from "@/shadcn/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shadcn/components/ui/select";
+import { useState } from "react";
 
 export default function Product({ params }: { params: { slug: string } }) {
+  const [pincode, setPincode] = useState("");
+  const [serviceable, setServiceable] = useState(-1);
+
+  const handlePincode = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(pincode);
+    const pins = await fetch(`http://localhost:3000/api/pincode?p=${pincode}`, { method: "GET" });
+    const pinJson = await pins.json();
+    if (pinJson.serviceablePincodes.includes(pincode)) {
+      setServiceable(1);
+    } else {
+      setServiceable(0);
+    }
+  };
+
   return (
     <section className="text-gray-400 overflow-hidden dark bg-background">
       <div className="container px-5 py-24 mx-auto">
@@ -12,43 +30,43 @@ export default function Product({ params }: { params: { slug: string } }) {
             <h1 className="text-gray-100 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
             <div className="flex mb-4">
               <span className="flex items-center">
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
                 <span className="text-gray-400 ml-3">4 Reviews</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-800 space-x-2s">
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                   </svg>
                 </a>
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
                 </a>
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                   </svg>
                 </a>
               </span>
             </div>
             <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-900 mb-5">
+            <div className="flex mt-6 pb-5 items-center border-gray-900">
               <div className="flex">
                 <span className="mr-3">Color</span>
                 <button className="border-2 border-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
@@ -70,13 +88,30 @@ export default function Product({ params }: { params: { slug: string } }) {
                 </Select>
               </div>
             </div>
+            <form className="flex flex-col border-b-2 mb-5 pb-5 gap-2" onSubmit={handlePincode}>
+              <div className="flex flex-row gap-2">
+                <Input
+                  type="text"
+                  placeholder="Enter Delivery Pincode"
+                  className="max-w-xs"
+                  value={pincode}
+                  onChange={(e) => {
+                    setPincode(e.target.value);
+                  }}
+                />
+                <Button variant={"secondary"} type="submit">
+                  Check
+                </Button>
+              </div>
+              {serviceable !== -1 ? <p className={`ml-1`}>{serviceable === 1 ? "Item is Deliverable to your location." : "Sorry! this item is not deliverable to your location."}</p> : null}
+            </form>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-100">$58.00</span>
               <Button variant={"default"} className="ml-auto">
                 Add to Cart
               </Button>
               <button className="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                 </svg>
               </button>
