6d68cfa26849b5e0b9fa15e58062418523363dc5
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-05 01:38:25 +0530

    fix: remnant code

--- src/app/api/auth/deleteuser/route.ts
+++ src/app/api/auth/deleteuser/route.ts
@@ -1,20 +1,17 @@
 import connectDb from "@/middleware/mongoose";
-import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
+import { decodeJwt } from "jose";
+import { NextRequest, NextResponse } from "next/server";
 import User from "../../../../../models/User";
 
-export const POST = async (req: Request) => {
+export const POST = async (req: NextRequest) => {
   await connectDb();
 
-  const authorizationHeader = req.headers.get("Authorization");
-  const token = authorizationHeader?.split(" ")[1] as string;
+  const token = req.cookies.get("token")?.value as string;
+
   let userTakeoutData;
 
   try {
-    if (!jwt.verify(token, process.env.JWT_SECRET as jwt.Secret)) {
-      return NextResponse.json({ success: false, error: "No token provided" }, { status: 401 });
-    }
-    const userData: any = jwt.decode(token);
+    const userData: any = await decodeJwt(token);
     userTakeoutData = await User.findOneAndDelete({ email: userData.email });
   } catch (error) {
     return NextResponse.json({ success: false, error: `${error}` }, { status: 500 });
