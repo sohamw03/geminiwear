8a871343f4dc0698b8001429aeb92ba1e2cb01e0
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-19 03:46:14 +0530

    chore: trying different config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  runtime: "edge", // for Edge API Routes only
-  unstable_allowDynamic: [
-    // allows a single file
-    "./node_modules/next-auth/**",
-  ],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true, // Required for Next.js 13+
+      };
+      config.module.rules.push({
+        test: /\.\/node_modules\/next\-auth\/node_modules\/@babel\/runtime\/regenerator\/index\.js/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["babel-plugin-import-dynamic"], // Might be necessary
+          },
+        },
+      });
+    }
+
+    return config;
+  },
+  unstable_allowDynamic: ["./node_modules/next-auth/react/index.js"], // Add additional paths if needed
 };
 
 module.exports = nextConfig;
