53d1ab158c11fc90c8d471d47afeaf5d0bcf743a
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-19 03:37:57 +0530

    fix: one line ignore next-auth dynamic code eval

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   runtime: "edge", // for Edge API Routes only
   unstable_allowDynamic: [
     // allows a single file
-    "./node_modules/next-auth/node_modules/@babel/runtime/regenerator/index.js",
-    "./node_modules/next-auth/react/index.js",
+    "./node_modules/next-auth/**",
   ],
 };
 
