e29e32feff68b92ffd8e1bb4c7ddb431a8b666d9
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-21 02:08:14 +0530

    fix: fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true, // Required for Next.js 13+
-      };
-      config.module.rules.push({
-        test: /\.\/node_modules\/next\-auth\/node_modules\/@babel\/runtime\/regenerator\/index\.js/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: ["babel-plugin-import-dynamic"], // Might be necessary
-          },
-        },
-      });
-    }
-
-    return config;
-  },
-  unstable_allowDynamic: ["./node_modules/next-auth/react/index.js"], // Add additional paths if needed
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
-import { getSession } from "next-auth/react";
-import { NextRequest, NextResponse } from "next/server";
+import withAuth, { NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/api/auth")) {
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req: NextRequestWithAuth) {
+    console.log(req.nextauth.token);
+
+    if (req.nextUrl.pathname.startsWith("/api/auth")) {
+      return NextResponse.next();
+    }
+
+    if (!req.nextauth.token) {
+      return NextResponse.rewrite(new URL("/login", req.url));
+    }
+
     return NextResponse.next();
   }
-
-  const requestForNextAuth = {
-    headers: Object.fromEntries(req.headers.entries()),
-  };
-  const session = await getSession({ req: requestForNextAuth });
-  if (!session?.user) {
-    return NextResponse.rewrite(new URL("/login", req.url));
-  }
-
-  return NextResponse.next();
-}
+);
 
 export const config = {
   matcher: ["/api/:path*", "/myaccount", "/checkout", "/order", "/orders"],
