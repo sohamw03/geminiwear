9b6d84da30b09381657e8f4960fa83b1cae7bb98
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-21 02:16:03 +0530

    fix: make login api good

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,26 +1,26 @@
 // DEPRECATED
 // ---------------------------------------------------------------------------------
-// import connectDb from "@/middleware/mongoose";
-// import { NextResponse } from "next/server";
-// import User from "../../../../../models/User";
+import connectDb from "@/middleware/mongoose";
+import { NextResponse } from "next/server";
+import User from "../../../../../models/User";
 
-// export const POST = async (req: Request) => {
-//   await connectDb();
-//   const { method } = req;
-//   const { email, password }: any = await req.json();
+export const POST = async (req: Request) => {
+  await connectDb();
+  const { method } = req;
+  const { email, password }: any = await req.json();
 
-//   if (method !== "POST") {
-//     return NextResponse.json({ success: false, error: "Invalid method" }, { status: 405 });
-//   }
+  if (method !== "POST") {
+    return NextResponse.json({ success: false, error: "Invalid method" }, { status: 405 });
+  }
 
-//   try {
-//     const existingUser = await User.findOne({ email: email });
-//     if (existingUser && existingUser.validPassword(password)) {
-//       return NextResponse.json({ success: true, message: `User login successful`, data: { name: existingUser.name, email } });
-//     } else {
-//       throw new Error("Email or password is incorrect");
-//     }
-//   } catch (error) {
-//     return NextResponse.json({ success: false, error: `${error}` }, { status: 200 });
-//   }
-// };
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser && existingUser.validPassword(password)) {
+      return NextResponse.json({ success: true, message: `User login successful`, data: { name: existingUser.name, email } });
+    } else {
+      throw new Error("Email or password is incorrect");
+    }
+  } catch (error) {
+    return NextResponse.json({ success: false, error: `${error}` }, { status: 200 });
+  }
+};
